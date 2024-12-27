311c57f49a91b27fdcdcef3e4b4d5ea3a17ec072
SharanRP <z8903830@gmail.com>
2024-12-27 20:00:30 +0530

    feat(analytics): add posthog integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Montserrat } from 'next/font/google';
+import { PostHogProvider } from '@/components/providers/PostHogProvider'
+import { PostHogPageview } from '@/components/PostHogPageview'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,7 +36,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased`}
       >
-        {children}
+        <PostHogProvider>
+          <PostHogPageview />
+          {children}
+        </PostHogProvider>
       </body>
     </html>
   );
