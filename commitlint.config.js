9b9066cee3c50392300040e438e3a5d6840c0109
SharanRP <z8903830@gmail.com>
2024-12-28 23:08:21 +0530

    chore: update commitlint configuration and branch name validation in github actions workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', [
-      'feat',
-      'fix',
-      'docs',
-      'style',
-      'refactor',
-      'perf',
-      'test',
-      'chore',
-      'revert',
-      'ci',
-      'build'
-    ]],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']
+    ],
     'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never']
+    'subject-case': [2, 'always', 'lower-case']
   }
 }; 
